e668f8e6a5a896f8c5d32b8e027fdcea5abbd974
Matthew Harris Glover <mglover@carnegielearning.com>
2024-06-15 15:22:23 -0400

    fix requirements

---
+++ models.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""mercury radio database models"""
+
+from tortoise import fields
+from tortoise.models import Model
+
+# pylint: disable=invalid-str-returned,missing-docstring
+
+class User(Model):
+    """track users"""
+    spotifyid = fields.TextField()
+    token = fields.BinaryField()
+    last_active = fields.DatetimeField(auto_now=True)
+    active_now = fields.TextField()
+
+    def __str__(self):
+        return self.spotifyid
+
+
+class Track(Model):
+    """track tracks"""
+    trackid = fields.CharField(max_length=255)
+    trackname = fields.TextField()
+    trackuri = fields.CharField(max_length=255)
+    duration_ms = fields.IntField()
+
+    def __str__(self):
+        return self.trackname
+
+
+class Rating(Model):
+    """track likes"""
+    userid = fields.TextField()
+    trackid = fields.TextField()
+    trackname = fields.TextField()
+    rating = fields.IntField()
+    last_played = fields.DatetimeField(auto_now=True)
+
+    def __str__(self):
+        return self.trackname
+
+
+class PlayHistory(Model):
+    """track the history of songs played"""
+    trackid = fields.TextField()
+    played_at = fields.DatetimeField(auto_now=True)
+
+    def __str__(self):
+        return self.trackid
+
+
+class UpcomingQueue(Model):
+    """track the upcoming songs"""
+    trackid = fields.TextField()
+    queued_at = fields.DatetimeField(auto_now=True)
