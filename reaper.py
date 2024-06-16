544d7a540123d98fa9a894966bcf48aecf1eb603
Matthew Harris Glover <mglover@carnegielearning.com>
2024-06-15 22:42:39 -0400

    reap every 5 minutes, not 10

--- reaper.py
+++ reaper.py
@@ -9,9 +9,9 @@ from models import User
 async def user_reaper():
     """check the database every 5 minutes and remove inactive users"""
     procname="user_reaper"
-    sleep = 600
+    sleep = 300
     while True:
-        logging.info("%s checking for inactive users every %s seconds", procname, sleep)
+        logging.debug("%s checking for inactive users every %s seconds", procname, sleep)
         interval = datetime.datetime.now() - datetime.timedelta(minutes=20)
         inactives  = await User.filter(last_active__lte=interval)
         for user in inactives:
