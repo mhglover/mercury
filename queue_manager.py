affb864e3d3658c570a0b5086daca6fcd0244c9f
Matthew Harris Glover <mglover@carnegielearning.com>
2024-08-03 08:08:23 -0400

    fix(get_request): fail gracefully back to a popular request

--- queue_manager.py
+++ queue_manager.py
@@ -60,22 +60,20 @@ async def queue_manager(spotify, cred, sleep=10):
             
             logging.info("%s getting a [%s] recommendation", procname, playtype)
             # pick the next track to add to the queue
+            if playtype == "request":
+                track = await get_request(spotify, cred)
+                if not track:
+                    logging.debug("%s no request track returned, switching to popular", procname)
+                    playtype = "popular"
+            
             if playtype == "fresh":
                 track = await get_fresh_tracks()
                 
-            elif playtype == "spotrec":
+            if playtype == "spotrec":
                 track = await spotrec_tracks(spotify)
             
-            elif playtype == "popular":
+            if playtype == "popular":
                 track = await popular_tracks()
-            
-            elif playtype == "request":
-                track = await get_request(spotify, cred)
-            
-            else:
-                logging.error("%s invalid playtype [%s] returned", procname, playtype)
-                # dropping out of the while loop, try again later
-                break
 
             logging.info("%s adding [%s] recommendation: %s", procname, playtype, track.trackname)
             
